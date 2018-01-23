d6ca7975b907198005c06f57d9ea9b667d70cd88
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-23 08:31:30 +0100

    optimize limit.c

--- exercises/set1/limit.c
+++ exercises/set1/limit.c
@@ -4,17 +4,41 @@
 int
 main(void)
 {
-	int n = 10;
-	int i = 0; /* TODO omit initializations? */
-	double lim = 0.0;
+	int n; /* TODO initialize all vs not? */
+	int i;
+	double lim;
+	int sign;
+	double denom; /* TODO fastest: int mult vs float mult? */
+	double coeff;
 
 	/* 1 - 1/2^2 + 1/2^4 - 1/2^6 + ... */
 	/* +1/2^0 - 1/2^2 + 1/2^4 - 1/2^6 + ... */
+	/* +1/1 - 1/4 + 1/16 - 1/64 + ... */
 	/* sum_0^n (-1)^i 1/2^(2i) */
 
-	lim = 0.0;
-	for (i = 0; i <= n; i++)
-		lim += pow(-1, i) * 1 / pow(2, 2*i);
+	n = 10;
+	lim = 0;
+	sign = 1;
+	denom = 1;
+	coeff = 1;
+	/* TODO define @ declare vs use? */
+	for (i = 0; i <= n; i++) {
+		/* lim += pow(-1, i) * 1 / pow(2, 2*i); */
+
+		/* lim += sign * 1 / pow(2, 2*i); */
+
+		/* lim += sign * 1 / denom; */
+
+		/*
+		lim += sign / denom;
+		denom *= 4;
+		*/
+		(void)denom;
+
+		lim += sign * coeff;
+		coeff *= 0.25;
+		sign *= -1;
+	}
 
 	printf("4/5 = %f\n", 4.0/5.0);
 	printf("limit = %f\n", lim);
