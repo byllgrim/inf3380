f8b3bfa32f3f53527e692333a2ea8df26000eeb0
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-25 16:36:43 +0100

    read farbfeld and write as greyscale

--- exercises/set1/ff2grey.c
+++ exercises/set1/ff2grey.c
@@ -1,6 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+struct image {
+	uint32_t width;
+	uint32_t height;
+	uint16_t *data; /* row-major greyscale values */
+};
 
 void
 swapend32(char *b)
@@ -15,23 +22,99 @@ swapend32(char *b)
 	b[3] = ((char *)&n)[0];
 }
 
-int
-main(void)
+void
+swapend16(char *b)
 {
-	char magic[8+1] = {0};
+	uint16_t n;
+
+	n = *(uint16_t *)b;
+
+	b[0] = ((char *)&n)[1];
+	b[1] = ((char *)&n)[0];
+}
+
+uint16_t
+greyavg(uint16_t *rgba)
+{
+	uint32_t sum = 0;
+
+	swapend16((char *)&rgba[0]);
+	swapend16((char *)&rgba[1]);
+	swapend16((char *)&rgba[2]);
+
+	sum += 2*rgba[0] + 3*rgba[1] + 1*rgba[2];
+	fprintf(stderr, "%x %x %x\n", rgba[0], rgba[1], rgba[2]);
+	fprintf(stderr, "%x\n", sum/6);
+
+	return sum / 6;
+}
+
+void
+readrow(uint16_t *row, size_t width)
+{
+	uint16_t *buf;
+	size_t bufsiz;
+	size_t i;
+	uint16_t pxl;
+	uint16_t alpha = 0xFFFF;
+
+	bufsiz = 4 * sizeof(*buf); /* RGBA */
+	buf = malloc(bufsiz);
+
+	for (i = 0; i < width; i++) {
+		read(STDIN_FILENO, buf, bufsiz);
+		pxl = greyavg(buf);
+		swapend16((char *)&pxl);
+		write(STDOUT_FILENO, &pxl, sizeof(pxl));
+		write(STDOUT_FILENO, &pxl, sizeof(pxl));
+		write(STDOUT_FILENO, &pxl, sizeof(pxl));
+		write(STDOUT_FILENO, &alpha, sizeof(alpha));
+	}
+	(void)row; /* TODO assign to array */
+}
+
+void
+readallrows(struct image image)
+{
+	size_t i;
+
+	for (i = 0; i < image.height; i++) {
+		readrow(&(image.data[i * image.width]), image.width);
+	}
+}
+
+struct image
+readff()
+{
+	struct image image;
+	char magic[8+1];
 	uint32_t width;
 	uint32_t height;
 
 	read(STDIN_FILENO, magic, sizeof(magic) - 1);
-	printf("magic = '%s'\n", magic);
+	magic[sizeof(magic) - 1] = '\0';
+	write(STDOUT_FILENO, magic, 8);
 
 	read(STDIN_FILENO, &width, sizeof(width));
+	write(STDOUT_FILENO, &width, 4);
 	swapend32((char *)&width);
-	printf("width = %d\n", width);
+	image.width = width; /* TODO omit redundant variables? */
 
 	read(STDIN_FILENO, &height, sizeof(height));
+	write(STDOUT_FILENO, &height, 4);
 	swapend32((char *)&height);
-	printf("height = %d\n", height);
+	image.height = height; /* TODO omit redundant variables? */
+
+	image.data = malloc(width * height * sizeof(*(image.data)));
+	readallrows(image);
+
+	return image;
+}
+
+int
+main(void)
+{
+	readff(); /* TODO why refactor? */
 
 	return 0;
 }
