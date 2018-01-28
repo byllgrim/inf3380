665bff8115b2d3d2affa8d04ebb4822bc1d5df63
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-28 21:38:15 +0100

    [permutation] complete ex 3

--- exercises/set1/permutation.c
+++ exercises/set1/permutation.c
@@ -33,16 +33,44 @@ sort(int arr[], int beg, int end)
 	sort(arr, r, end);
 }
 
+void
+sortperm(int arr[], int perm[], int beg, int end)
+{
+	int piv;
+	int l;
+	int r;
+
+	if (end <= beg + 1)
+		return;
+
+	piv = arr[perm[beg]];
+	l = beg + 1;
+	r = end;
+	for (; l < r; ) {
+		if (arr[perm[l]] <= piv)
+			l++;
+		else
+			swap(&perm[l], &perm[--r]);
+	}
+
+	swap(&perm[--l], &perm[beg]);
+	sortperm(arr, perm, beg, l);
+	sortperm(arr, perm, r, end);
+}
+
 int
 main(void)
 {
 	size_t i;
 	int arr[] = {5, 2, 3, 1, 4, 6};
+	int perm[] = {0, 1, 2, 3, 4, 5};
 
-	sort(arr, 0, 5);
-
+	sortperm(arr, perm, 0, 5);
 	for (i = 0; i < 6; i++)
 		printf("[%02d] %02d\n", i, arr[i]);
+	printf("\n");
+	for (i = 0; i < 6; i++)
+		printf("[%02d] %02d\n", i, arr[perm[i]]);
 
 	return 0;
 }
