3891a5a8624c83e5eae5f421329c8e3f77173af1
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-28 21:12:00 +0100

    [permutation] fix coding style

--- exercises/set1/permutation.c
+++ exercises/set1/permutation.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
-void swap(int *a, int *b)
+void
+swap(int *a, int *b)
 {
-	int t=*a; *a=*b; *b=t;
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
 
-void sort(int arr[], int beg, int end)
+void
+sort(int arr[], int beg, int end)
 {
-	if (end > beg + 1) {
-		int piv = arr[beg], l = beg + 1, r = end;
-		while (l < r) {
-			if (arr[l] <= piv)
+	int piv;
+	int l;
+	int r;
+
+	if (end <= beg + 1)
+		return;
+
+	piv = arr[beg];
+	l = beg + 1;
+	r = end;
+	for (; l < r; ) {
+		if (arr[l] <= piv)
 			l++;
-			else
+		else
 			swap(&arr[l], &arr[--r]);
-		}
-		swap(&arr[--l], &arr[beg]);
-		sort(arr, beg, l);
-		sort(arr, r, end);
 	}
+
+	swap(&arr[--l], &arr[beg]);
+	sort(arr, beg, l);
+	sort(arr, r, end);
 }
 
 int
