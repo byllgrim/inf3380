2c27ad8e6633d287f7c58fc8c62e69b6279aea52
byllgrim <robin.a.t.pedersen@gmail.com>
2018-02-01 00:28:08 +0100

    [binary] read binary and ascii dat files

--- exercises/set2/binary.c
+++ exercises/set2/binary.c
@@ -27,12 +27,12 @@ generatedata(size_t len)
 }
 
 void
-writebinary(uint16_t* buf, size_t len)
+writebinary(uint16_t* buf, size_t len, char *filename)
 {
 	int fd;
 	size_t nb;
 
-	fd = open("binary.dat", O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (fd == -1) {
 		fprintf(stderr, "writebinary: open failed\n");
 		exit(EXIT_FAILURE);
@@ -49,12 +49,12 @@ writebinary(uint16_t* buf, size_t len)
 }
 
 void
-writeascii(uint16_t* buf, size_t len)
+writeascii(uint16_t* buf, size_t len, char *filename)
 {
 	FILE *file;
 	size_t i;
 
-	file = fopen("ascii.dat", "w");
+	file = fopen(filename, "w");
 	if (!file) {
 		fprintf(stderr, "writeascii: failed to open file\n");
 		exit(EXIT_FAILURE);
@@ -66,14 +66,69 @@ writeascii(uint16_t* buf, size_t len)
 	fclose(file);
 }
 
+void
+readbinary(char *filename)
+{
+	int fd;
+	uint16_t *buf;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		fprintf(stderr, "readbinary: failed to open file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	buf = malloc(BUFSIZ);
+	if (!buf) {
+		fprintf(stderr, "readbinary: mallocing buf failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (; read(fd, buf, BUFSIZ) > 0;)
+		;
+
+	free(buf);
+	close(fd);
+}
+
+void
+readascii(char *filename)
+{
+	FILE *file;
+	int *buf;
+	size_t i;
+
+	file = fopen(filename, "r");
+	if (!file) {
+		fprintf(stderr, "readascii: failed to open file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	buf = malloc(BUFSIZ);
+	if (!buf) {
+		fprintf(stderr, "readascii: failed to malloc buf\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; !feof(file); i++)
+		fscanf(file, "%05d", &buf[i]);
+
+	free(buf);
+	fclose(file);
+}
+
 int
 main(void)
 {
 	uint16_t *data = (void *)0;
+	char *binname = "binary.dat";
+	char *asciiname = "ascii.dat";
 
 	data = generatedata(DATALEN);
-	writebinary(data, DATALEN);
-	writeascii(data, DATALEN);
+	writebinary(data, DATALEN, binname);
+	writeascii(data, DATALEN, asciiname);
+	readbinary(binname);
+	readascii(asciiname);
 
 	if (data)
 		free(data);
