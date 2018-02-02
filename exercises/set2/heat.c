e7413d4b2e90e96c2132dd22bfc7190654d4d7d9
byllgrim <robin.a.t.pedersen@gmail.com>
2018-02-02 11:15:22 +0100

    begin heat solver

---
+++ exercises/set2/heat.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+void
+heatsolve(void)
+{
+	/* TODO */
+}
+
+int
+main(void)
+{
+	float *u;
+	size_t nx = 1;
+	size_t ny = 1;
+	size_t nz = 1;
+
+	u = malloc(nx * ny * nz * sizeof(*u));
+
+	return 0;
+}
